cb6298b1a5576ddaffd2cf51a966218229c895db
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2026-06-03 15:11:52 -0700

    feat: remove Studio & Show route with redirect

--- src/routes/studio-show.tsx
+++ src/routes/studio-show.tsx
@@ -1,18 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
-import { PageHeading } from '@/components/page-heading'
-import { STUDIO_SHOW_SEO } from '@/lib/legacy-seo'
-import { createSeoHead } from '@/lib/seo'
+// import { PageHeading } from '@/components/page-heading'
+// import { STUDIO_SHOW_SEO } from '@/lib/legacy-seo'
+// import { createSeoHead } from '@/lib/seo'
 
 export const Route = createFileRoute('/studio-show')({
-  head: () => createSeoHead(STUDIO_SHOW_SEO),
-  component: StudioShowRoute,
+  beforeLoad: () => {
+    throw redirect({ to: '/about', statusCode: 301 })
+  },
+  // head: () => createSeoHead(STUDIO_SHOW_SEO),
+  // component: StudioShowRoute,
 })
 
-function StudioShowRoute() {
-  return (
-    <main className="pb-20">
-      <PageHeading title="Studio & Show" />
-    </main>
-  )
-}
+// function StudioShowRoute() {
+//   return (
+//     <main className="pb-20">
+//       <PageHeading title="Studio & Show" />
+//     </main>
+//   )
+// }
